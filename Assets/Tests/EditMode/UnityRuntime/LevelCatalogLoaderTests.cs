6359e6b4e498dae2066627c7952304fec7c48fb8
Aggimalla Abhishek <aggimallaabhishek@gmail.com>
2026-06-16 08:43:20 +0530

    refactor: balance moves, goals, and star thresholds for all MVP levels

--- Assets/Tests/EditMode/UnityRuntime/LevelCatalogLoaderTests.cs
+++ Assets/Tests/EditMode/UnityRuntime/LevelCatalogLoaderTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Linq;
 using PotionPopQuest.Core;
 using PotionPopQuest.Unity;
 
@@ -14,6 +15,29 @@ namespace PotionPopQuest.Unity.Tests
             Assert.That(levels.Count, Is.GreaterThanOrEqualTo(20));
             Assert.That(levels[0].LevelNumber, Is.EqualTo(1));
             Assert.That(levels[levels.Count - 1].LevelNumber, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void LoadLevels_ObstacleGoalsDoNotExceedPlacedObstacles()
+        {
+            var levels = new LevelCatalogLoader(new NullGameLogger()).LoadLevels(null);
+
+            foreach (var level in levels)
+            {
+                foreach (var goal in level.Goals)
+                {
+                    if (goal.GoalType != GoalType.BreakObstacle && goal.GoalType != GoalType.ClearTile)
+                    {
+                        continue;
+                    }
+
+                    var available = level.Obstacles.Count(obstacle => obstacle.ObstacleType == goal.Obstacle);
+                    Assert.That(
+                        available,
+                        Is.GreaterThanOrEqualTo(goal.Amount),
+                        $"Level {level.LevelNumber} asks for {goal.Amount} {goal.Obstacle} goals but only places {available}.");
+                }
+            }
         }
     }
 }
